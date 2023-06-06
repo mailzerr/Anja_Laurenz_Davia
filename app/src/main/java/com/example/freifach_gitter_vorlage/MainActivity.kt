c8442a6239c98024a3ab205d827168ba8dafff52
itsmeOnO <star.anny14@gmail.com>
2023-06-06 16:36:04 +0200

    Hello

--- app/src/main/java/com/example/freifach_gitter_vorlage/MainActivity.kt
+++ app/src/main/java/com/example/freifach_gitter_vorlage/MainActivity.kt
@@ -31,50 +31,42 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val imageButton6: ImageButton = findViewById(R.id.imageButton6)
         imageButton6.setOnClickListener(this)
     }
+
     override fun onClick(v: View) {
-        var score = 1
-        var max = 100
         when (v.id) {
-            R.id.imageButton1 -> {replaceImageWithDelay(R.id.imageButton1)
-                score = score + 1 }
+            R.id.imageButton1 -> replaceImageWithDelay(R.id.imageButton1)
             //showToast("Button 1 geklickt")
-            R.id.imageButton2 -> {replaceImageWithDelay(R.id.imageButton2)
-                score = score + 1}
+            R.id.imageButton2 -> replaceImageWithDelay(R.id.imageButton2)
             //showToast("Button 2 geklickt")
-            R.id.imageButton3 -> {replaceImageWithDelay(R.id.imageButton3)
-                score = score + 1}
+            R.id.imageButton3 -> replaceImageWithDelay(R.id.imageButton3)
             //showToast("Button 3 geklickt")
-            R.id.imageButton4 -> {replaceImageWithDelay(R.id.imageButton4)
-                score = score + 1}
+            R.id.imageButton4 -> replaceImageWithDelay(R.id.imageButton4)
             //showToast("Button 4 geklickt")
-            R.id.imageButton5 -> {replaceImageWithDelay(R.id.imageButton5)
-                score = score + 1}
+            R.id.imageButton5 -> replaceImageWithDelay(R.id.imageButton5)
             //showToast("Button 5 geklickt")
-            R.id.imageButton6 -> {replaceImageWithDelay(R.id.imageButton6)
-                score = score + 1}
+            R.id.imageButton6 -> replaceImageWithDelay(R.id.imageButton6)
             //showToast("Button 6 geklickt")
         }
-        if (score == max){
-            showToast("Score 100 erreicht")
-        }
+
     }
+
     private fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
     private fun replaceImageWithDelay(resID: Int) {
         // Referenz zum ImageView, das ersetzt werden soll
         val imageViewToReplace: ImageButton = findViewById(resID)
-        imageViewToReplace.setImageResource(R.drawable.ohne_maus)
+        imageViewToReplace.setImageResource(R.drawable.mit_maus)
 
         // Referenz zum ImageView, das das ursprüngliche ImageView ersetzen soll
         val replacementImageView: ImageButton = findViewById(resID)
-        replacementImageView.setImageResource(R.drawable.mit_maus)
+        replacementImageView.setImageResource(R.drawable.ohne_maus)
 
         // Handler erstellen
         val handler = Handler()
 
         // Minimale und maximale Verzögerungszeit in Millisekunden
-        val minDelay = 500
+        val minDelay = 900
         val maxDelay = 1500
 
         // Zufällige Verzögerungszeit generieren
@@ -83,14 +75,16 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 // Verzögerten Task ausführen
         handler.postDelayed({
             // Hier wird das Bild des zu ersetzenden ImageView geändert
-            imageViewToReplace.setImageResource(R.drawable.ohne_maus)
+            imageViewToReplace.setImageResource(R.drawable.mit_maus)
 
             // Zufällige Verzögerungszeit generieren
             val randomDelay2 = Random.nextInt(minDelay, maxDelay).toLong()
 
             // Handler erneut ausführen, um das Bild wieder zu ändern
             handler.postDelayed({
-                imageViewToReplace.setImageResource(R.drawable.mit_maus)
+                imageViewToReplace.setImageResource(R.drawable.ohne_maus)
             }, randomDelay2) // Hier wird die zufällige Verzögerungszeit verwendet
         }, randomDelay) }// Hier wird die zufällige Verzögerungszeit verwendet
+
+
 }
