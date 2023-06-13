e8e84bc5202e77cfef2d5025c22fc7318a4760a7
Laupfau <laupfau@outlook.com>
2023-06-13 17:57:45 +0200

    imageButton2-6 in val hinzugefügt

--- app/src/main/java/com/example/freifach_gitter_vorlage/MainActivity.kt
+++ app/src/main/java/com/example/freifach_gitter_vorlage/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.view.View
 import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
@@ -31,29 +32,46 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val imageButton6: ImageButton = findViewById(R.id.imageButton6)
         imageButton6.setOnClickListener(this)
     }
+    var score = 1
+    var max = 100
     override fun onClick(v: View) {
-        var score = 1
-        var max = 100
+
         when (v.id) {
-            R.id.imageButton1 -> {replaceImageWithDelay(R.id.imageButton1)
-                score = score + 1 }
+            R.id.imageButton1 -> {//if (R.id.imageButton1.)
+
+                replaceImageWithDelay(R.id.imageButton1)
+
+                score = score + 1
+            }
             //showToast("Button 1 geklickt")
             R.id.imageButton2 -> {replaceImageWithDelay(R.id.imageButton2)
-                score = score + 1}
+                score = score + 1
+
+            }
             //showToast("Button 2 geklickt")
             R.id.imageButton3 -> {replaceImageWithDelay(R.id.imageButton3)
-                score = score + 1}
+                score = score + 1
+
+            }
             //showToast("Button 3 geklickt")
             R.id.imageButton4 -> {replaceImageWithDelay(R.id.imageButton4)
-                score = score + 1}
+                score = score + 1
+
+            }
             //showToast("Button 4 geklickt")
             R.id.imageButton5 -> {replaceImageWithDelay(R.id.imageButton5)
-                score = score + 1}
+                score = score + 1
+
+            }
             //showToast("Button 5 geklickt")
             R.id.imageButton6 -> {replaceImageWithDelay(R.id.imageButton6)
-                score = score + 1}
+                score = score + 1
+
+            }
             //showToast("Button 6 geklickt")
         }
+        val textView: TextView = findViewById(R.id.textview) as TextView
+        textView.text = score.toString() //+ "StrafPunkte:"
         if (score == max){
             showToast("Score 100 erreicht")
         }
@@ -64,11 +82,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private fun replaceImageWithDelay(resID: Int) {
         // Referenz zum ImageView, das ersetzt werden soll
         val imageViewToReplace: ImageButton = findViewById(resID)
-        imageViewToReplace.setImageResource(R.drawable.ohne_maus)
+        imageViewToReplace.setImageResource(R.drawable.mit_maus)
 
         // Referenz zum ImageView, das das ursprüngliche ImageView ersetzen soll
         val replacementImageView: ImageButton = findViewById(resID)
-        replacementImageView.setImageResource(R.drawable.mit_maus)
+        replacementImageView.setImageResource(R.drawable.ohne_maus)
 
         // Handler erstellen
         val handler = Handler()
@@ -83,14 +101,14 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 // Verzögerten Task ausführen
         handler.postDelayed({
             // Hier wird das Bild des zu ersetzenden ImageView geändert
-            imageViewToReplace.setImageResource(R.drawable.ohne_maus)
+            imageViewToReplace.setImageResource(R.drawable.mit_maus)
 
             // Zufällige Verzögerungszeit generieren
             val randomDelay2 = Random.nextInt(minDelay, maxDelay).toLong()
 
             // Handler erneut ausführen, um das Bild wieder zu ändern
             handler.postDelayed({
-                imageViewToReplace.setImageResource(R.drawable.mit_maus)
+                imageViewToReplace.setImageResource(R.drawable.ohne_maus)
             }, randomDelay2) // Hier wird die zufällige Verzögerungszeit verwendet
         }, randomDelay) }// Hier wird die zufällige Verzögerungszeit verwendet
 }
